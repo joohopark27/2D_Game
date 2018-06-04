6e030523c0ee492546d0385911809d08d000c268
Jonathan Park <33333270+joohopark27@users.noreply.github.com>
2018-06-04 11:18:01 -0400

    Create Util.java

---
+++ src/org/joohopark/Util.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Util{
+
+    public static String loadMap (String path){
+        String Map = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Map.txt"));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Map += line;
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Map;
+
+    }
+
+    public static int parseInt(String number){
+		try{
+			return Integer.parseInt(number);
+		}catch(NumberFormatException e){
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+}
