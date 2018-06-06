1ed0e0a094eaecd27d027af799e9cf2eb06615d6
Jonathan Park <joohopark27@gmail.com>
2018-06-06 17:59:34 -0400

    update

--- src/org/joohopark/Game.java
+++ src/org/joohopark/Game.java
@@ -2,8 +2,38 @@ package org.joohopark;
 
 public class Game {
 
+	Clock clock;
+	
+	boolean isRunning;
+	
+	public Game(){
+		
+		clock = new Clock();
+		
+		init();
+		run();
+		
+	}
+	
+	public void init(){
+		
+		isRunning = true;
+		
+	}
+	
+	public void run(){
+		while(isRunning){
+			if(clock.timer()){
+				update();
+			}
+		}
+	}
+	
+	public void update(){
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
