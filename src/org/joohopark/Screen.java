2e12be73092bbcce872531732d52f3d4601ac80d
Jonathan Park <joohopark27@gmail.com>
2018-06-11 22:19:01 -0400

    shows map on editor state

--- src/org/joohopark/Screen.java
+++ src/org/joohopark/Screen.java
@@ -1,6 +1,7 @@
 package org.joohopark;
 
 import org.joohopark.Assets.Tiles;
+import org.joohopark.State.State;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,10 +12,6 @@ public class Screen extends Canvas{
 	private static final long serialVersionUID = 1L;
 	
 	private JFrame frame;
-	private Canvas canvas;
-	
-	private BufferStrategy bs;
-	private Graphics g;
 	
 	public Screen(String Title, Dimension dimension){
 
@@ -26,41 +23,8 @@ public class Screen extends Canvas{
 		frame.setVisible(true);
 		frame.setFocusable(true);
 		
-		canvas = new Canvas();
-		canvas.setPreferredSize(dimension);
-		canvas.setMaximumSize(dimension);
-		canvas.setMinimumSize(dimension);
-		canvas.setFocusable(false);
-		
-		//frame.add(canvas);
-		//frame.pack();
-		
 	}
-	
-	public void updateScreen(int[][] map){
-		
-		bs = canvas.getBufferStrategy();
-		if(bs == null){
-			canvas.createBufferStrategy(3);
-			return;
-		}
-		
-		g = bs.getDrawGraphics();
-		
-		g.clearRect(0, 0, WIDTH, HEIGHT);
 
-        for(int y = 0; y < map.length; y++){
-            for(int x = 0; x < map[y].length; x++){
-                g.drawImage(Tiles.getTile(map[y][x]), x * 10, y * 10, 10, 10, null);
-            }
-        }
-
-		//end drawing
-		bs.show();
-		g.dispose();
-		
-	}
-	
 	public JFrame getFrame(){
 		return frame;
 	}
--- src/org/joohopark/State/EditorState.java
+++ src/org/joohopark/State/EditorState.java
@@ -1,17 +1,40 @@
 package org.joohopark.State;
 
+import org.joohopark.Assets.Tiles;
 import org.joohopark.Game;
-import org.joohopark.Util.Util;
-import org.joohopark.World.EditWorld;
+import org.joohopark.World.World;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class EditorState extends State{
 
+    private Canvas canvas;
+
+    private BufferStrategy bs;
+    private Graphics g;
+
+    private World world;
+
     public EditorState(Game game){
         super(game);
+        world = new World(game.getMapName());
+
+        Dimension dimension = new Dimension(screen.getWidth(), screen.getHeight());
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(dimension);
+        canvas.setMaximumSize(dimension);
+        canvas.setMinimumSize(dimension);
+        canvas.setFocusable(false);
+
+        screen.add(canvas);
+        screen.pack();
+    }
+
+    @Override
+    public void reset() {
+
     }
 
     @Override
@@ -20,8 +43,27 @@ public class EditorState extends State{
     }
 
     @Override
-    public void render() {
+    public void render(){
 
+        bs = canvas.getBufferStrategy();
+        if(bs == null){
+            canvas.createBufferStrategy(3);
+            return;
+        }
+
+        g = bs.getDrawGraphics();
+
+        g.clearRect(0, 0, screen.getWidth(), screen.getHeight());
+
+        for(int y = 0; y < world.getyLength(); y++){
+            for(int x = 0; x < world.getxLength(); x++){
+                g.drawImage(Tiles.getTile(world.getMap()[y][x]), x * 10, (y - 4) * 10, 10, 10, null);
+            }
+        }
+
+        //end drawing
+        bs.show();
+        g.dispose();
 
     }
 }
